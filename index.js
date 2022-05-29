89be9d863de9a9d6a37a2ae48175132c7844983a
Daniel Yahalom <daniel.yahalom@gmail.com>
2022-05-29 17:06:11 -0500

    initial commit ported everything from the next backend

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const express = require('express');
+import cors from 'cors';
+import express from 'express';
+import redis from 'redis';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+app.use(cors());
+
+var db = redis.createClient({url: process.env.DATABASE_URL});
+await db.connect();
+
+app.get('/search', async (req, res) => {
+    res.status(200);
+    if (!req.query.q)
+    {
+        res.json([{ title: "NO DATA YET!" }]);
+        return;
+    }
+
+        //send response from datbase:
+        let output = await db.sMembers("documents");
+        var responses = [];
+        output.forEach((value, index) => {
+            if(value.toUpperCase().includes(req.query["q"].toUpperCase()))
+            {
+                responses.push({title: value});
+            }
+        });
+        res.json(responses);
+
 });
 
 app.listen(port, () => {
